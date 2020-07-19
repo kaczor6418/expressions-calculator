7ba0f507aa9007dcdaf2f8d2889ee5b1ed183df2
kaczor6418 <k.kaczynski97@gmail.com>
2020-07-19 23:51:31 +0200

    chore: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,37 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
   entry: './src/index.ts',
+  output: {
+    publicPath: '',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: 'ts-loader',
+        include: [
+            path.resolve(__dirname, 'src')
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          'css-loader'
+        ],
         include: [
             path.resolve(__dirname, 'src')
         ]
@@ -22,13 +45,16 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
+    contentBase: path.join(__dirname, 'dist')
   },
-  output: {
-    publicPath: 'dist',
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ]
 };
