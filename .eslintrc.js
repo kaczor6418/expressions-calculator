27124015c6346f888d7e5a9ef74ea242af487ffd
kaczor6418 <k.kaczynski97@gmail.com>
2020-07-26 14:19:32 +0200

    chore(code-quality): add prettier and eslint-typescript

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        createDefaultProgram: true,
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended', // Make sure this is always the last configuration in the extends array.
+    ],
+    rules: {
+        '@typescript-eslint/no-namespace': 0,
+        '@typescript-eslint/no-inferrable-types': 0,
+    },
+};
