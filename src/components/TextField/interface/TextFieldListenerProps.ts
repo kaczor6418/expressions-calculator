3f86f3f12eb6ad893b5ad2462541e04e62eb7f29
kaczor6418 <k.kaczynski97@gmail.com>
2020-08-01 18:03:56 +0200

    fix(typing): remove any type from TextFieldListenerProps interface

--- src/components/TextField/interface/TextFieldListenerProps.ts
+++ src/components/TextField/interface/TextFieldListenerProps.ts
@@ -1,18 +1,16 @@
 export type TextFieldListenerProps = BlurTextFieldListenerProps | InputTextFieldListenerProps;
 
 export interface BaseTextFieldListenerProps {
-    eventName: any; //Extract<keyof GlobalEventHandlersEventMap, 'blur' | 'input'>;
-    callback(e?: FocusEvent | InputEvent): void;
+    eventName: Extract<keyof GlobalEventHandlersEventMap, 'blur' | 'input'>;
+    callback: EventListener;
 }
 
 export interface BlurTextFieldListenerProps extends BaseTextFieldListenerProps {
-    eventName: any; //Extract<keyof GlobalEventHandlersEventMap, 'blur'>;
-    callback(e?: FocusEvent): void;
+    eventName: Extract<keyof GlobalEventHandlersEventMap, 'blur'>;
 }
 
 export interface InputTextFieldListenerProps extends BaseTextFieldListenerProps {
-    eventName: any; //Extract<keyof GlobalEventHandlersEventMap, 'input'>;
-    callback(e?: InputEvent): void;
+    eventName: Extract<keyof GlobalEventHandlersEventMap, 'input'>;
 }
 
 export function isBlurTextFieldProps(value: BaseTextFieldListenerProps): value is BlurTextFieldListenerProps {
